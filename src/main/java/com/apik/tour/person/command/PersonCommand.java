e0c94c0aa89d0c624d450a3f5928b79362d53d18
GD-Seok <wizardry0629@naver.com>
2017-10-10 20:58:51 +0900

    Revert "spring 기초 파일과 view 폴더 경로 추가"
    This reverts commit dd46d59b8db19458d4a373a6d676c16ea34b34bc.

--- src/main/java/com/apik/tour/person/command/PersonCommand.java
+++
@@ -1,6 +0,0 @@
-package com.apik.tour.person.command;
-
-//Person Table을 공통으로 사용할 때 쓰는 DTO 파일 입니다.
-public class PersonCommand {
-
-}
--- src/main/java/com/apik/tour/person/dao/PersonDao.java
+++
@@ -1,6 +0,0 @@
-package com.apik.tour.person.dao;
-
-//Person Table의 동작을 설정하는 DAO 선언 부분 입니다.
-public interface PersonDao {
-
-}
