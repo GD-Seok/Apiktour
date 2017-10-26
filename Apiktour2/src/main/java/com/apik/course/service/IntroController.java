a2434122d33933061bc4498ef24d10b8a9eceba3
jihui bak <jiiezing@gmail.com>
2017-10-26 11:17:55 +0900

    intro_controller
    intro_controller 수정부분

--- Apiktour2/src/main/java/com/apik/course/service/IntroController.java
+++ Apiktour2/src/main/java/com/apik/course/service/IntroController.java
@@ -18,81 +18,72 @@ import com.apik.course.dao.CourseDAO;
 public class IntroController {
 
 	// 1) Log객체 생성
-		//private Logger log=Logger.getLogger(ListController.class);
-		private Logger log=Logger.getLogger(this.getClass());
-	
-		@Autowired // 자동으로 객체를 구해와 불러옴
-		private CourseDAO courseDao;	
-		
+	// private Logger log=Logger.getLogger(ListController.class);
+	private Logger log = Logger.getLogger(this.getClass());
+
+	@Autowired // 자동으로 객체를 구해와 불러옴
+	private CourseDAO courseDao;
+
 	@RequestMapping("/index.do")
 	public String process() {
 		return "intro/intro";
 	}
+
+	@RequestMapping(value = "/choicePlay.do", method = RequestMethod.GET)
+	public String choicePlay( Model model) {
+		String type="에너지";
+		String mode = "play"; 
+			
+		if (log.isDebugEnabled()) { // 현재 로그객체가 디버그모드상태라면
+			System.out.println("===choice() 호출!===");
+			log.debug("mode : " + mode); // 내부적인 처리과정
+			System.out.println("===choice() end===");
+		}		
+		
+		model.addAttribute("mode", mode);
+		model.addAttribute("type", type);
+		
+		return "intro/choicePlay";
+	}
 	
-	
-	@RequestMapping(value="/choice.do",method=RequestMethod.GET)
-	public String choice(@RequestParam String mode, Model model) {
+	@RequestMapping(value = "/choiceRest.do", method = RequestMethod.GET)
+	public String choiceRest(Model model) {
+		String type="피로도";
+		String mode = "rest"; 
 		
-		String type="";
-		
-		String m = mode; 
-		switch(m) { 
-		case "rest": type="피로도"; break; 
-		case "play": type="에너지"; break; 
-		}
-		
-		// Log객체를 이용하여 처리과정 콘솔에 출력
 		if (log.isDebugEnabled()) { // 현재 로그객체가 디버그모드상태라면
 			System.out.println("===choice() 호출!===");
 			log.debug("mode : " + mode); // 내부적인 처리과정
 			System.out.println("===choice() end===");
 		}
 		
-		System.out.println("===choice() 호출!===");
-		System.out.println("mode : " + mode);
-		System.out.println("===choice() end===");
-		
 		model.addAttribute("mode", mode);
 		model.addAttribute("type", type);
 		
-		return "intro/choice"; 
+		return "intro/choiceRest";
 	}
-	
-	
-	
-	/*@RequestMapping("/choicePro.do")
-	public String pro() {
-		return "intro/playPro";
-	}
-	*/
-	
-	
-	@RequestMapping("/choicePro.do")
+
+	@RequestMapping("/playPro.do") 
 	public ModelAndView process(
 			@RequestParam(value="mode", defaultValue="") String mode,
-			@RequestParam(value="level", defaultValue="") String level
-			) {
+			@RequestParam(value="level", defaultValue="") String level) {
 		
-		String type="";		
+		String type="";
+		String m = mode;
 		
-		String m = mode; 
 		switch(m) { 
-		case "rest": type="피로도"; break; 
 		case "play": type="에너지"; break; 
 		}
 		
 		int lv = Integer.parseInt(level);
 		String play=mode;
-
-		// DB 조회 : 매개변수를 받아 해당하는 코스 개수 조회
+		
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("play", play);
 		map.put("lv", new Integer(lv));
 		
 		int count=courseDao.getCourseCount(map);
-				
 		
-		// Log객체를 이용하여 처리과정 콘솔에 출력
 		if (log.isDebugEnabled()) { // 현재 로그객체가 디버그모드상태라면
 			System.out.println("===choicePro() 호출!===");
 			log.debug("mode : " + mode); // 내부적인 처리과정
@@ -101,23 +92,69 @@ public class IntroController {
 			log.debug("count : " + count);	
 			System.out.println("===choicePro() end===");
 		}
-
-		System.out.println("===choicePro() 호출!===");		
-		System.out.println("play : " + play);
-		System.out.println("lv : " + lv); 
-		System.out.println("count : " + count);
-		System.out.println("===choicePro() end===");
 		
 		ModelAndView mav = new ModelAndView();		
-		mav.setViewName("intro/playPro"); //intro/playPro.jsp
+		mav.setViewName("intro/playPro");
 		mav.addObject("count", count);
 		mav.addObject("type", type);
 		mav.addObject("mode", mode);
 		mav.addObject("level", level);
 		mav.addObject("play", play);
 		
+		System.out.println("count : " + count);
+		System.out.println("type : " + type);
+		System.out.println("mode : " + mode);
+		System.out.println("level : " + level);
+		System.out.println("play : " + play);
+		
 		return mav;
 	}
 	
+	@RequestMapping("/restPro.do") 
+	public ModelAndView proceess(
+			@RequestParam(value="mode", defaultValue="") String mode,
+			@RequestParam(value="level", defaultValue="") String level) {
+		
+		String type="";
+		String m = mode;
+		
+		switch(m) { 
+		case "rest": type="피로도"; break; 
+		}
+		
+		int lv = Integer.parseInt(level);
+		String rest=mode;
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("rest", rest);
+		map.put("lv", new Integer(lv));
+		
+		int count=courseDao.getCourseCount(map);
+		
+		if (log.isDebugEnabled()) { // 현재 로그객체가 디버그모드상태라면
+			System.out.println("===choicePro() 호출!===");
+			log.debug("mode : " + mode); // 내부적인 처리과정
+			log.debug("play : " + rest);
+			log.debug("lv : " + lv);
+			log.debug("count : " + count);	
+			System.out.println("===choicePro() end===");
+		}
+		
+		ModelAndView mav = new ModelAndView();		
+		mav.setViewName("intro/playPro");
+		mav.addObject("count", count);
+		mav.addObject("type", type);
+		mav.addObject("mode", mode);
+		mav.addObject("level", level);
+		mav.addObject("play", rest);
+		
+		System.out.println("count : " + count);
+		System.out.println("type : " + type);
+		System.out.println("mode : " + mode);
+		System.out.println("level : " + level);
+		System.out.println("play : " + rest);
+		
+		return mav;
+	}
 	
 }
